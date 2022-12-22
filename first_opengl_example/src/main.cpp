ae1889fe13f14a548965a02a933bec86dfc7f908
akito <emilrtx07@gmail.com>
2022-12-23 07:43:52 +0900

    CMake 디렉토리 옮긴 자료

--- first_opengl_example/src/main.cpp
+++ first_opengl_example/src/main.cpp
@@ -1,5 +1,6 @@
 #include <spdlog/spdlog.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 
 int main(int argc, const char** argv){
@@ -13,15 +14,30 @@ int main(int argc, const char** argv){
         SPDLOG_ERROR("failed to initialize glfw: {}", description);
         return -1;
     }
+
+    // ... glfwInit() 호출 후
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfw 윈도우 생성, 실패하면 에러 출력후 종료
-    SPDLOG_INFO("Create glfw window");
-    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HIGHT, WINDOW_NAME, nullptr, nullptr);
-    if (!window) {
-        SPDLOG_ERROR("failed to create glfw window");
+    	SPDLOG_INFO("Create glfw window");
+        auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+        if (!window) {
+            SPDLOG_ERROR("failed to create glfw window");
+            glfwTerminate();
+            return -1;
+        }
+        glfwMakeContextCurrent(window);
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_ERROR("failed to initialize glad");
         glfwTerminate();
         return -1;
     }
-    
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version: {}", glVersion);
+
     SPDLOG_INFO("Start main loop");
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
